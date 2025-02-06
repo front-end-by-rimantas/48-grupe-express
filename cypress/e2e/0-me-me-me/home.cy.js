e584d4599cd32a6def53972993d1fe628b6870d1
Rimantas <belauzas@gmail.com>
2025-02-06 10:51:58 +0200

    Up

--- cypress/e2e/0-me-me-me/home.cy.js
+++ cypress/e2e/0-me-me-me/home.cy.js
@@ -19,7 +19,67 @@ describe('Home page', () => {
         cy.get('footer > p')
             .contains('Company');
 
+        // cy.get('footer > p')
+        //     .contains(new Date().getFullYear());
+    });
+
+
+    it('Hero image', () => {
+        cy.visit('http://localhost:4811/');
+
+        cy.get('main section img')
+            .should('be.exist')
+            .should('be.visible')
+            .and('have.prop', 'naturalWidth')
+            .should('be.greaterThan', 0);
+    });
+
+    it('Header links', () => {
+        cy.visit('http://localhost:4811/');
+
+        cy.get('header > ul > li')
+            .contains('Home')
+            .should('have.attr', 'href', '/');
+
+        cy.get('header > ul > li')
+            .contains('About')
+            .should('have.attr', 'href', '/about');
+
+        cy.get('header > ul > li')
+            .contains('Services')
+            .should('have.attr', 'href', '/services');
+
+        cy.get('header > ul > li')
+            .contains('Calc')
+            .should('have.attr', 'href', '/calc');
+    });
+
+    it('Footer links', () => {
+        cy.visit('http://localhost:4811/');
+
         cy.get('footer > p')
-            .contains(new Date().getFullYear());
+            .should('be.exist')
+            .should('be.visible')
+            .should('have.text', '© 2024 Company, Inc');
+
+        cy.get('footer > ul > li')
+            .contains('Home')
+            .should('have.attr', 'href', '/');
+
+        cy.get('footer > ul > li')
+            .contains('Features')
+            .should('have.attr', 'href', '/features');
+
+        cy.get('footer > ul > li')
+            .contains('Pricing')
+            .should('have.attr', 'href', '/pricing');
+
+        cy.get('footer > ul > li')
+            .contains('FAQs')
+            .should('have.attr', 'href', '/faq');
+
+        cy.get('footer > ul > li')
+            .contains('About')
+            .should('have.attr', 'href', '/about');
     });
 });
